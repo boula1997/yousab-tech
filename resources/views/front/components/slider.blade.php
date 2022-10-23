f6e50f67424e1b1e2aa466b2eb2d4de85cfee7a1
fsd5802 <fsd5802@gmail.com>
2022-10-23 11:28:38 +0200

    3rd commit

---
+++ resources/views/front/components/slider.blade.php
@@ -0,0 +1,46 @@
+<header class="slider-main">
+    <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-ride="carousel">
+        <ol class="carousel-indicators">
+           <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+           <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+           <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+        </ol>
+        <div class="carousel-inner" role="listbox">
+           <!-- Slide One - Set the background image for this slide in the line below -->
+           <div class="carousel-item active" style="background-image: url('images/slider-01.jpg')">
+              <div class="carousel-caption d-none d-md-block">
+                 <h3>مرحبا بكم في موقع زهرة البستان</h3>
+                 <p>شركة زهرة البستان شركة رائدة في مجال تنسيق الحدائق منذ أكثر من 10أعوام حيث أثبتت جدارتها في جميع  </p>
+                 <p>أنحاء المملكة بالخدمات التي تقدمها  حيث انها تمتلك فريق عمل مدرب من الأداريين والمهندسين والمصممين!</p>
+                 <p>والفنين والعمال علي أعلي مستوي لتنفيذ أصعب مشاريع تنسيق حدائق الفلل  والقصور وجميع أعمال اللأند سكيب</p>
+              </div>
+           </div>
+           <!-- Slide Two - Set the background image for this slide in the line below -->
+           <div class="carousel-item" style="background-image: url('images/slider-02.jpg')">
+              <div class="carousel-caption d-none d-md-block">
+                 <h3>مرحبا بكم في موقع زهرة البستان</h3>
+                 <p>شركة زهرة البستان شركة رائدة في مجال تنسيق الحدائق منذ أكثر من 10أعوام حيث أثبتت جدارتها في جميع  </p>
+                 <p>أنحاء المملكة بالخدمات التي تقدمها  حيث انها تمتلك فريق عمل مدرب من الأداريين والمهندسين والمصممين!</p>
+                 <p>والفنين والعمال علي أعلي مستوي لتنفيذ أصعب مشاريع تنسيق حدائق الفلل  والقصور وجميع أعمال اللأند سكيب</p>
+              </div>
+           </div>
+           <!-- Slide Three - Set the background image for this slide in the line below -->
+           <div class="carousel-item" style="background-image: url('images/slider-03.jpg')">
+              <div class="carousel-caption d-none d-md-block">
+                 <h3>مرحبا بكم في موقع زهرة البستان</h3>
+                 <p>شركة زهرة البستان شركة رائدة في مجال تنسيق الحدائق منذ أكثر من 10أعوام حيث أثبتت جدارتها في جميع  </p>
+                 <p>أنحاء المملكة بالخدمات التي تقدمها  حيث انها تمتلك فريق عمل مدرب من الأداريين والمهندسين والمصممين!</p>
+                 <p>والفنين والعمال علي أعلي مستوي لتنفيذ أصعب مشاريع تنسيق حدائق الفلل  والقصور وجميع أعمال اللأند سكيب</p>
+              </div>
+           </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">السابق</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">التالي</span>
+        </a>
+    </div>
+</header>
---
+++ resources/views/front/index.blade.php
@@ -0,0 +1,8 @@
+@extends('front.layouts.master')
+@section('content')
+    @include('front.components.slider')
+    @include('front.components.services')
+    @include('front.components.about')
+    @include('front.components.portfolio')
+    @include('front.components.contact')
+@endsection
