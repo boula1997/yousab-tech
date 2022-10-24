ec907a33c008839b77d95cc73f9b1749c0c10352
fsd5802 <fsd5802@gmail.com>
2022-10-24 15:35:47 +0200

    adding login and register

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ServiceController;
-use App\Http\Controllers\BlogController;
-use App\Http\Controllers\GalleryController;
-use App\Http\Controllers\ContactController;
-use App\Http\Controllers\AboutController;
-use App\Http\Controllers\SettingController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,40 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('services', ServiceController::class);
-Route::resource('blogs', BlogController::class);
-Route::resource('settings', SettingController::class);
-Route::resource('home', HomeController::class);
-Route::resource('portfolio', GalleryController::class);
-Route::resource('contact', ContactController::class);
-Route::resource('about', AboutController::class);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-
-Route::get('routes', function () {
-    $routeCollection = Route::getRoutes();
-    
-    echo "<table style='width:100%'>";
-    echo "<tr>";
-    echo "<td width='10%'><h4>HTTP Method</h4></td>";
-    echo "<td width='10%'><h4>Route</h4></td>";
-    echo "<td width='10%'><h4>Name</h4></td>";
-    echo "<td width='70%'><h4>Corresponding Action</h4></td>";
-    echo "</tr>";
-    foreach ($routeCollection as $value) {
-        echo "<tr>";
-        echo "<td>" . $value->methods()[0] . "</td>";
-        echo "<td>" . $value->uri() . "</td>";
-        echo "<td>" . $value->getName() . "</td>";
-        echo "<td>" . $value->getActionName() . "</td>";
-        echo "</tr>";
-    }
-    echo "</table>";
-});
-
-// Route::get('/',[HomeController::class, 'index'])->name('home');
-// Route::get('/about',[AboutController::class, 'index'])->name('about');
-// Route::get('/services',[ServiceController::class, 'index'])->name('service');
-// Route::get('/service/{id}',[ServiceController::class, 'show'])->name('service-details');
-// Route::get('/portfolios',[GalleryController::class, 'index'])->name('portfolio');
-// Route::get('/contact',[ContactController::class, 'index'])->name('contact');
-// Route::post('/contact_form',[ContactController::class, 'contact_post'])->name('direct_contact_post');
+require __DIR__.'/auth.php';
