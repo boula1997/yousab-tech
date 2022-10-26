9edb85d9dcac3062c0ab3554eb6d903aee863a04
fsd5802 <fsd5802@gmail.com>
2022-10-26 15:04:57 +0200

    page adding

--- app/Models/Setting.php
+++ app/Models/Setting.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Setting extends Model
 {
     use HasFactory;
-    protected $fillable = ['logo','tab','address','description','map','title','image','facebook','twitter','youtube','tiktok','instgram','phone1','phone2','phone3'];
+    protected $fillable = ['logo','tab','address','map','title','image','facebook','twitter','youtube','tiktok','instgram','phone1','phone2','phone3'];
 
 }
