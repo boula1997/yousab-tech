f005c626b66cba98ec51dc6fa7e57dd991243a91
fsd5802 <fsd5802@gmail.com>
2022-10-31 16:59:19 +0200

    commit

--- resources/views/front/components/contact.blade.php
+++ resources/views/front/components/contact.blade.php
@@ -14,7 +14,7 @@
         </p>
 
         <p>{{ $setting->phone1 }}</p>
-        <a href="https://wa.me/966500774150"><i style="font-size:200%" class="fab fa-whatsapp"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="tel:00966500774150"><i style="font-size:200%" class="fas fa-phone-square"></i></a><br>
+        <p><a href="https://wa.me/966500774150"><i style="font-size:200%" class="fab fa-whatsapp"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="tel:00966500774150"><i style="font-size:200%" class="fas fa-phone-square"></i></a></p> 
                     
         <p>{{ $setting->phone2 }}</p>
         <a href="https://wa.me/966537394580"><i style="font-size:200%"class="fab fa-whatsapp"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="tel:00966537394580"><i style="font-size: 200%" class="fas fa-phone-square"></i></a><br>
