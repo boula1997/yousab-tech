a8b2d9965ec50c0d92d11a62a6a0ffb7a3ab52c3
fsd5802 <fsd5802@gmail.com>
2022-10-31 15:34:49 +0200

    new commit

--- resources/views/front/components/portfolio.blade.php
+++ resources/views/front/components/portfolio.blade.php
@@ -3,13 +3,15 @@
     <h2>سابقة الاعمال</h2>
 
     <div class="row">
-        @foreach ($portfolios as $portfolio)
+       
             <div class="col-lg-4 mb-4">
                 {{-- <div class="card h-100">
                     <h4 class="card-header">{{ $portfolio->title }}</h4> --}}
                     <div class="card-img">
+                        @foreach ($portfolios as $portfolio)
                         <a href="{{ asset($portfolio->image) }}" data-lightbox="portfolio" data-title="{{ $portfolio->title }}"><img class="img-fluid" src="{{ asset($portfolio->image) }}" alt="" /></a>
                         
+                        @endforeach
                     </div>
                     {{-- <div class="card-body">
                         {!! $portfolio->description !!}
@@ -19,7 +21,6 @@
                     </div> --}}
                 {{-- </div> --}}
             </div>
-        @endforeach
     </div>
     <!-- /.row -->
 </div>
