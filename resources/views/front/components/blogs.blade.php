36073c961f16d29796df19b9cfe43dbfe584b1e6
fsd5802 <fsd5802@gmail.com>
2022-10-31 13:26:50 +0200

    commit

--- resources/views/front/components/blogs.blade.php
+++ resources/views/front/components/blogs.blade.php
@@ -1,24 +1,24 @@
-        <!-- Portfolio Section -->
-        <div class="portfolio-main">
-            <h2>المقالات</h2>
-            <div class="row">
-               @foreach ($blogs as $blog)
-               <div class="col-lg-4 col-sm-6 portfolio-item">
-                  <div class="card h-100">
-                     <div class="card-img">
-                        <a href="#">
-                           <img class="card-img-top" src="{{asset($blog->image)}}" alt="" />
-                           <div class="overlay"><i class="fas fa-arrows-alt"></i></div>
-                        </a>
-                     </div>
-                     <div class="card-body">
-                        <h4 class="card-title">
-                           <a href="{{route('blogs.show',$blog)}}">{{$blog->title}}</a>
-                        </h4>
-                     </div>
-                  </div>
-               </div>                 
-               @endforeach
+<div class="blogs-bar">
+    <!-- blogs Section -->
+    <h2>المقالات</h2>
+
+    <div class="row">
+      @foreach ($blogs as $blog)
+      <div class="col-lg-4 mb-4">
+         <div class="card h-100">
+            <h4 class="card-header">{{ $blog->title }}</h4>
+            <div class="card-img">
+               <img class="img-fluid" src="{{ asset($blog->image) }}" alt="" />
             </div>
-            <!-- /.row -->
-        </div>
+            <div class="card-body">
+               {!! $blog->description   !!}
+            </div>
+            <div class="card-footer">
+               <a href="{{route('blogs.show',$blog)}}" class="btn btn-primary">تصفح الخدمة</a>
+            </div>
+         </div>
+      </div>    
+      @endforeach
+    </div>
+    <!-- /.row -->
+</div>
--- resources/views/front/components/services.blade.php
+++ resources/views/front/components/services.blade.php
@@ -1,23 +1,24 @@
-<div class="services-bar">
+        <!-- Portfolio Section -->
+        <div class="portfolio-main">
     <h1 class="my-4">خدماتنا المميزة</h1>
-    <!-- Services Section -->
-    <div class="row">
-      @foreach ($services as $service)
-      <div class="col-lg-4 mb-4">
-         <div class="card h-100">
-            <h4 class="card-header">{{ $service->title }}</h4>
-            <div class="card-img">
-               <img class="img-fluid" src="{{ asset($service->image) }}" alt="" />
+            <div class="row">
+               @foreach ($services as $service)
+               <div class="col-lg-4 col-sm-6 portfolio-item">
+                  <div class="card h-100">
+                     <div class="card-img">
+                        <a href="#">
+                           <img class="card-img-top" src="{{asset($service->image)}}" alt="" />
+                           <div class="overlay"><i class="fas fa-arrows-alt"></i></div>
+                        </a>
+                     </div>
+                     <div class="card-body">
+                        <h4 class="card-title">
+                           <a href="{{route('services.show',$service)}}">{{$service->title}}</a>
+                        </h4>
+                     </div>
+                  </div>
+               </div>                 
+               @endforeach
             </div>
-            <div class="card-body">
-               {!! $service->description   !!}
-            </div>
-            <div class="card-footer">
-               <a href="{{route('services.show',$service)}}" class="btn btn-primary">تصفح الخدمة</a>
-            </div>
-         </div>
-      </div>    
-      @endforeach
-    </div>
-    <!-- /.row -->
-</div>
+            <!-- /.row -->
+        </div>
