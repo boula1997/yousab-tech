f53a1788dad245b654d8072b71ee071dad90659a
boula1997 <boula.nessim.soliman@gmail.com>
2022-11-05 17:45:18 +0200

    commit

--- resources/views/front/components/slider.blade.php
+++ resources/views/front/components/slider.blade.php
@@ -13,7 +13,7 @@
            <div class="carousel-item {{$loop->index==0?'active':''}}" style="background-image: url('{{asset($slider->image)}}')">
                 <div class="carousel-caption d-none d-md-block">
                     <div class="row">
-                        <div class="col-md-1">
+                        <div class="col-sm-1">
                             <ul class="social_footer_ul">
                                 <li style="margin-bottom: 150%"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
                                 <li style="margin-bottom: 150%"><a href="{{ $setting->twitter }}" target="_blank"><i class="fab fa-twitter"></i></a></li>
@@ -21,11 +21,11 @@
                                 <li style="margin-bottom: 150%"><a href="{{ $setting->instgram }}" target="_blank"><i class="fab fa-instagram"></i></a></li>
                             </ul>
                         </div>
-                        <div class="col-md-10" style="margin-top:10%">
+                        <div class="col-sm-10" style="margin-top:10%">
                             <h3>{{$slider->title}}</h3>
                             {!! $slider->description  !!}
                         </div>
-                        <div class="col-md-1">
+                        <div class="col-sm-1">
                             <ul class="social_footer_ul">
                                 <li style="margin-bottom: 150%"><a href="#"><i class="fab fa-facebook-f"></i></a></li>
                                 <li style="margin-bottom: 150%"><a href="{{ $setting->twitter }}" target="_blank"><i class="fab fa-twitter"></i></a></li>
