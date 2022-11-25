0449c81256c2dadef36fa70937ab27449292cebd
boula1997 <boula.nessim.soliman@gmail.com>
2022-11-25 22:41:58 +0200

    commit

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -18,7 +18,7 @@ class PageController extends Controller
     public function index()
     {
         $pages = Page::latest()->get();
-        return view('admin.crud.pages.Index', compact('Pages'))
+        return view('admin.crud.pages.Index', compact('pages'))
             ->with('i', (request()->input('page', 1) - 1) * 5);
     }
  
