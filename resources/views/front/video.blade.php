bb95a2078d242e77f22540d53ae21d62c4631af6
boula1997 <boula.nessim.soliman@gmail.com>
2022-11-26 20:35:49 +0200

    commit

--- resources/views/front/video.blade.php
+++ resources/views/front/video.blade.php
@@ -32,23 +32,7 @@
         {{-- {{dd($video->youtube_link)}} --}}
         <div class="col-md-4 col-sm-12 mb-3" style="height: 400px">
             <div style="height: 100%" class="rounded-5 card mb-3" >
-                <iframe class="rounded-5" width="100%" height="100%" src="https://www.youtube.com/embed/C0DPdy98e4c"
-                    title="YouTube video player" frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen></iframe>
-            </div>
-        </div>
-        <div class="col-md-4 col-sm-12 mb-3" style="height: 400px">
-            <div class="card" style="height: 100%">
-                <iframe class="rounded-5" width="100%" height="100%" src="https://www.youtube.com/embed/LXb3EKWsInQ"
-                    title="YouTube video player" frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen></iframe>
-            </div>
-        </div>
-        <div class="col-md-4 col-sm-12 mb-3" style="height: 400px">
-            <div class="card" style="height: 100%">
-                <iframe class="rounded-5" width="100%" height="100%" src="https://www.youtube.com/embed/CHSnz0bCaUk"
+                <iframe class="rounded-5" width="100%" height="100%" src="{{$video->youtube_link}}"
                     title="YouTube video player" frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowfullscreen></iframe>
