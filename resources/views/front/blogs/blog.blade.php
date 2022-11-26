866735705a4ad8ce8738d329b90b30994fc9acfd
kero-edward <kyrillos.edward17@gmail.com>
2022-11-26 19:53:58 +0200

    our works and articles pages

--- resources/views/front/blogs/blog.blade.php
+++ resources/views/front/blogs/blog.blade.php
@@ -1,22 +1,30 @@
+<style>
+    .hm-gradient {
+        background: #57FA57;
+        border-radius: 4px;
+    }
+
+</style>
+
+
 @extends('front.layouts.master')
 @section('content')
 	<!-- full Title -->
 	<div class="full-title">
-		<div class="container">
+		<div class="container text-center">
 			<!-- Page Heading/Breadcrumbs -->
 			<h1 class="mt-4 mb-3">المقالات
 			</h1>
 		</div>
 	</div>
     <div class="container">
-        <div class="breadcrumb-main">
-			<ol class="breadcrumb">
-				<li class="breadcrumb-item">
-					<a href="index.html">الرئسية</a>
-				</li>
-				<li class="breadcrumb-item active">{{$blog_section->title}}</li>
-			</ol>
-		</div>
+        <div class="container">
+            <div class="hm-gradient mb-5">
+                <div class="container p-4" style="font-size: 20px">
+                    <a style="color: #4E3A14" href="index.html">الرئيسية</a> / <span style="color: white">المقالات</span>
+                </div>
+            </div>
+        </div>
         <img class="img-fluid rounded mb-4" src="{{asset($blog_section->image)}}" alt="" />
         @include('front.components.blogs')
         @include('front.components.partners')
--- resources/views/front/portfolio.blade.php
+++ resources/views/front/portfolio.blade.php
@@ -1,23 +1,29 @@
+<style>
+    .hm-gradient {
+        background: #57FA57;
+        border-radius: 4px;
+    }
+
+</style>
+
 @extends('front.layouts.master')
 @section('content')
 	<!-- full Title -->
 	<div class="full-title">
-		<div class="container">
+		<div class="container text-center">
 			<!-- Page Heading/Breadcrumbs -->
 			<h1 class="mt-4 mb-3">سابقة الاعمال
 			</h1>
 		</div>
 	</div>
     <div class="container">
-        <div class="breadcrumb-main">
-			<ol class="breadcrumb">
-				<li class="breadcrumb-item">
-					<a href="index.html">الرئسية</a>
-				</li>
-				<li class="breadcrumb-item active">{{$portfolio_section->title}}</li>
-			</ol>
-		</div>
-        <img class="img-fluid rounded mb-4" src="{{asset($portfolio_section->image)}}" alt="" />
+        <div class="hm-gradient mb-5">
+            <div class="container p-4" style="font-size: 20px">
+                <a style="color: #4E3A14" href="index.html">الرئيسية</a> / <span
+                    style="color: white">{{$portfolio_section->title}}</span>
+            </div>
+        </div>
+        {{-- <img class="img-fluid rounded mb-4" src="{{asset($portfolio_section->image)}}" alt="" /> --}}
         @include('front.components.portfolio')
         @include('front.components.partners')
     </div>
