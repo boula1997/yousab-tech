d10d01930ff5f697936c7eb0207b9970097ff1de
boula1997 <boula.nessim.soliman@gmail.com>
2022-12-03 22:10:14 +0200

    commit

--- resources/views/admin/crud/sliders/edit.blade.php
+++ resources/views/admin/crud/sliders/edit.blade.php
@@ -7,42 +7,42 @@
                 <h2>اضف صور للمعرض</h2>
             </div>
             <div class="pull-left">
-                <a class="btn btn-primary" href="{{ route('sliders.index') }}" title="Go back"> <i class="fas fa-backward "></i>
-                </a>
+                <a class="btn btn-primary" href="{{route('sliders.index')}}" title="Go back"> <i class="fas fa-backward "></i> </a>
             </div>
         </div>
     </div>
 
     @if ($message = Session::get('success'))
-        <div class="alert alert-success">
-            <p style="text-align: end">{{ $message }} </p>
-        </div>
-    @endif
+    <div class="alert alert-success">
+        <p style="text-align: end">{{$message}} </p>
+    </div>
+@endif
 
     @if ($errors->any())
         <div class="alert alert-danger">
-            <strong>اخطاء!</strong>
+            <strong>اخطاء!</strong> 
             <ul>
                 @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
+                <li>{{$error}}</li>
                 @endforeach
             </ul>
         </div>
     @endif
-    <form action="{{ route('sliders.update', $slider) }}" method="POST" enctype="multipart/form-data">
+    <form action="{{route('sliders.update',$slider)}}" method="POST" enctype="multipart/form-data">
         @csrf
         @method('PUT')
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>الاسم:</strong>
-                    <input type="text" value="{{ old('title', $slider->title) }}" name="title" class="form-control">
+                    <input type="text" value="{{old('title',$slider->title)}}" name="title" class="form-control">
                 </div>
             </div>
             <div class="col-xs-12 col-sm-12 col-md-12">
                 <div class="form-group">
                     <strong>الوصف:</strong>
-                    <textarea class="form-control" style="height:30%" name="description">{{ old('description', $slider->description) }}</textarea>
+                    <textarea class="form-control" style="height:30%" name="description"
+                      >{{old('description',$slider->description)}}</textarea>
                 </div>
             </div>
 
@@ -50,41 +50,31 @@
                 <div class="card card-custom">
                     <div class="card-body">
                         <div class="row">
-                            <div class="col-md-12">
-                                <img height="50%" src="{{ asset('public/' . $slider->image) }}" alt="">
+                            <div class="col-md-10">
+                                <img height="50%" src="{{asset('public/'.$slider->image)}}" alt="">
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-md-12">
+                            <div class="col-md-2">
                                 <div class="form-group" style="text-align: center">
-                                    <label class="col-form-label" style="font-size: 15pt"> <strong> اختر
-                                            الصور</strong></label>
+                                    <label class="col-form-label" style="font-size: 15pt"> <strong> اختر الصور</strong></label>
                                     <div class="image-input image-input-empty image-input-outline" id="kt_image_5">
                                         <div class="image-input-wrapper"></div>
-                                        <label
-                                            class="btn btn-xs btn-icon btn-circle btn-white btn-hover-text-primary btn-shadow"
-                                            data-action="change" data-toggle="tooltip" title=""
-                                            data-original-title="Change avatar">
+                                        <label class="btn btn-xs btn-icon btn-circle btn-white btn-hover-text-primary btn-shadow" data-action="change" data-toggle="tooltip" title="" data-original-title="Change avatar">
                                             <i class="fa fa-pen icon-sm text-muted"></i>
                                             <input type="file" name="image" accept=".png, .jpg, .jpeg, .svg" />
                                             <input type="hidden" name="profile_avatar_remove" />
                                         </label>
-                                        <span
-                                            class="btn btn-xs btn-icon btn-circle btn-white btn-hover-text-primary btn-shadow"
-                                            data-action="cancel" data-toggle="tooltip" title="Cancel avatar">
+                                        <span class="btn btn-xs btn-icon btn-circle btn-white btn-hover-text-primary btn-shadow" data-action="cancel" data-toggle="tooltip" title="Cancel avatar">
                                             <i class="ki ki-bold-close icon-xs text-muted"></i>
                                         </span>
-                                        <span
-                                            class="btn btn-xs btn-icon btn-circle btn-white btn-hover-text-primary btn-shadow"
-                                            data-action="remove" data-toggle="tooltip" title="Remove avatar">
+                                        <span class="btn btn-xs btn-icon btn-circle btn-white btn-hover-text-primary btn-shadow" data-action="remove" data-toggle="tooltip" title="Remove avatar">
                                             <i class="ki ki-bold-close icon-xs text-muted"></i>
                                         </span>
                                     </div>
                                 </div>
-
+            
                             </div>
+             
                         </div>
-
                     </div>
                 </div>
             </div>
