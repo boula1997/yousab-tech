b6e92b2eeae1518606e7ad1bd27c5f76a9627431
fsd5802 <fsd5802@gmail.com>
2023-05-09 14:53:02 +0300

    adding form blade to components

--- resources/views/admin/crud/blogs/create.blade.php
+++ resources/views/admin/crud/blogs/create.blade.php
@@ -38,59 +38,46 @@
                         </div>
                         <!-- /.card-header -->
 
-                        {{-- validation messages start --}}
-                        @if ($errors->any())
-                            <div class="alert alert-danger">
-                                <strong>اخطاء!</strong>
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
+
+                        @section('form_content')
+                            <!-- form start -->
+
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Name</label>
+                                <input type="text" name="title" value="{{ old('title') }}" class="form-control"
+                                    id="exampleInputName" placeholder="Enter Name">
                             </div>
-                        @endif
-                        {{-- validation messages end --}}
 
-                        <!-- form start -->
-                        <form action="{{ route('blogs.store') }}" method="POST" enctype="multipart/form-data">
-                            @csrf
-                            <div class="card-body">
-                                <div class="form-group">
-                                    <label for="exampleInputEmail1">Name</label>
-                                    <input type="text" name="title" value="{{ old('title') }}"
-                                        class="form-control" id="exampleInputName" placeholder="Enter Name">
-                                </div>
+                            <div class="form-group">
+                                <label for="exampleInputDescription">Description</label>
+                                <textarea id="summernote" name="description">
+                            </textarea>
+                            </div>
 
-                                <div class="form-group">
-                                    <label for="exampleInputDescription">Description</label>
-                                    <textarea id="summernote" name="description">
-                                        </textarea>
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="exampleInputFile">File input</label>
-                                    <div class="input-group">
-                                        <div class="custom-file">
-                                            <input type="file" name="image" class="custom-file-input"
-                                                id="exampleInputFile">
-                                            <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                                        </div>
-                                        <div class="input-group-append">
-                                            <span class="input-group-text">Upload</span>
-                                        </div>
+                            <div class="form-group">
+                                <label for="exampleInputFile">File input</label>
+                                <div class="input-group">
+                                    <div class="custom-file">
+                                        <input type="file" name="image" class="custom-file-input"
+                                            id="exampleInputFile">
+                                        <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                                    </div>
+                                    <div class="input-group-append">
+                                        <span class="input-group-text">Upload</span>
                                     </div>
                                 </div>
-                                {{-- <div class="form-check">
-                                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                                    </div> --}}
                             </div>
-                            <!-- /.card-body -->
+                            {{-- <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                             </div> --}}
+
 
                             <div class="card-footer">
                                 <button type="submit" class="btn btn-primary">Submit</button>
                             </div>
-                        </form>
+                        @endsection
+
                     </div>
                     <!-- /.card -->
 
