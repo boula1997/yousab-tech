b5e9814e720ec4adfce4ad4627681a49a9dc496a
boula1997 <boula.nessim.soliman@gmail.com>
2023-06-26 21:24:29 +0300

    adding roles and permissions to site usiing spatie package

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use App\Http\Controllers\Admin\PageController;
 use App\Http\Controllers\Admin\ImageController;
 use App\Http\Controllers\Admin\CounterController;
 use App\Http\Controllers\Admin\VideoController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -33,68 +36,54 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/admin/login',[App\Http\Controllers\Auth\LoginController::class,'showAdminLoginForm'])->name('admin.login-view');
-Route::post('/admin/login',[App\Http\Controllers\Auth\LoginController::class,'adminLogin'])->name('admin.login');
+Route::get('/admin/login', [App\Http\Controllers\Auth\LoginController::class, 'showAdminLoginForm'])->name('admin.login-view');
+Route::post('/admin/login', [App\Http\Controllers\Auth\LoginController::class, 'adminLogin'])->name('admin.login');
 
-Route::get('/admin/register',[App\Http\Controllers\Auth\RegisterController::class,'showAdminRegisterForm'])->name('admin.register-view');
-Route::post('/admin/register',[App\Http\Controllers\Auth\RegisterController::class,'createAdmin'])->name('admin.register');
+Route::get('/admin/register', [App\Http\Controllers\Auth\RegisterController::class, 'showAdminRegisterForm'])->name('admin.register-view');
+Route::post('/admin/register', [App\Http\Controllers\Auth\RegisterController::class, 'createAdmin'])->name('admin.register');
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::get('/admin/dashboard',function(){
+Route::get('/admin/dashboard', function () {
     return view('dashboard');
 })->middleware('auth:admin');
 
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
 
-// Route::resource('home',HomeController::class);
-Route::resource('blogs',BlogController::class)->middleware(['auth']);
-// Route::resource('contact',ContactController::class)->middleware(['auth']);
-Route::resource('services',ServiceController::class)->middleware(['auth']);
-Route::resource('sliders',SliderController::class)->middleware(['auth']);
-Route::resource('pages',PageController::class)->middleware(['auth']);
-// Route::resource('about',AboutController::class)->middleware(['auth']);
-Route::resource('portfolios',PortfolioController::class)->middleware(['auth']);
-Route::resource('counters',CounterController::class)->middleware(['auth']);
-Route::resource('videos',VideoController::class)->middleware(['auth']);
-Route::resource('tests',ImageController::class)->middleware(['auth']);
-Route::put('/setting','App\Http\Controllers\Admin\SettingController@setting')->name('setting');
-Route::get('/setting/edit','App\Http\Controllers\Admin\SettingController@editSetting')->name('edit.setting');
+Route::put('/setting', 'App\Http\Controllers\Admin\SettingController@setting')->name('setting');
+Route::get('/setting/edit', 'App\Http\Controllers\Admin\SettingController@editSetting')->name('edit.setting');
 
-Route::get('/','App\Http\Controllers\SiteController@home')->name('front.home');
-Route::get('/blog-page','App\Http\Controllers\SiteController@blogs')->name('front.blog');
-Route::get('/contacts','App\Http\Controllers\SiteController@contact')->name('front.contact');
-Route::get('/service','App\Http\Controllers\SiteController@services')->name('front.service');
-Route::get('/single-service','App\Http\Controllers\SiteController@single_service')->name('front.show.service');
-Route::get('/single-blog','App\Http\Controllers\SiteController@single_blog')->name('front.show.blog');
-Route::get('/portfolio','App\Http\Controllers\SiteController@portfolios')->name('front.portfolio');
-Route::get('/video','App\Http\Controllers\SiteController@videos')->name('front.video');
-Route::get('/about','App\Http\Controllers\SiteController@about')->name('front.about');
-Route::post('/contact','App\Http\Controllers\SiteController@contact_post')->name('front.contact.post');
+Route::get('/', 'App\Http\Controllers\SiteController@home')->name('front.home');
+Route::get('/blog-page', 'App\Http\Controllers\SiteController@blogs')->name('front.blog');
+Route::get('/contacts', 'App\Http\Controllers\SiteController@contact')->name('front.contact');
+Route::get('/service', 'App\Http\Controllers\SiteController@services')->name('front.service');
+Route::get('/single-service', 'App\Http\Controllers\SiteController@single_service')->name('front.show.service');
+Route::get('/single-blog', 'App\Http\Controllers\SiteController@single_blog')->name('front.show.blog');
+Route::get('/portfolio', 'App\Http\Controllers\SiteController@portfolios')->name('front.portfolio');
+Route::get('/video', 'App\Http\Controllers\SiteController@videos')->name('front.video');
+Route::get('/about', 'App\Http\Controllers\SiteController@about')->name('front.about');
+Route::post('/contact', 'App\Http\Controllers\SiteController@contact_post')->name('front.contact.post');
 
 
-Route::get('routes', function () {
-    $routeCollection = Route::getRoutes();
-    
-    echo "<table style='width:100%'>";
-    echo "<tr>";
-    echo "<td width='10%'><h4>HTTP Method</h4></td>";
-    echo "<td width='10%'><h4>Route</h4></td>";
-    echo "<td width='10%'><h4>Name</h4></td>";
-    echo "<td width='70%'><h4>Corresponding Action</h4></td>";
-    echo "</tr>";       
-    foreach ($routeCollection as $value) {
-        echo "<tr>";
-        echo "<td>" . $value->methods()[0] . "</td>";
-        echo "<td>" . $value->uri() . "</td>";
-        echo "<td>" . $value->getName() . "</td>";
-        echo "<td>" . $value->getActionName() . "</td>";
-        echo "</tr>";
-    }
-    echo "</table>";
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('roles', RoleController::class);
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    // Route::resource('home',HomeController::class);
+    Route::resource('blogs', BlogController::class);
+    // Route::resource('contact',ContactController::class);
+    Route::resource('services', ServiceController::class);
+    Route::resource('sliders', SliderController::class);
+    Route::resource('pages', PageController::class);
+    // Route::resource('about',AboutController::class);
+    Route::resource('portfolios', PortfolioController::class);
+    Route::resource('counters', CounterController::class);
+    Route::resource('videos', VideoController::class);
+    Route::resource('tests', ImageController::class);
+
+    Route::resource('roles', RoleController::class);
+    Route::resource('users', UserController::class);
+    Route::resource('products', ProductController::class);
 });
-
-
