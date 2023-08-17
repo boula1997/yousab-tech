20ae70e4c5864cee6b6baadb7b3882be3d859794
boula1997 <boula.nessim.soliman@gmail.com>
2023-08-17 12:49:14 +0300

    adding api localization

--- routes/api.php
+++ routes/api.php
@@ -42,26 +42,32 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 // Route::get('page/{id}', 'PageController@show');
 // Route::post('store/contact', 'CContactController@store');
 
-Route::get('/services', [ServiceController::class, 'index']);
-Route::get('/service/{id}', [ServiceController::class, 'show']);
+Route::group(['middleware' => ['apiLocalization']], function () {
+    Route::get('/services', [ServiceController::class, 'index']);
+    Route::get('/service/{id}', [ServiceController::class, 'show']);
+    
+    Route::get('/blogs', [BlogController::class, 'index']);
+    Route::get('/blog/{id}', [BlogController::class, 'show']);
+    
+    Route::get('/sliders', [SliderController::class, 'index']);
+    Route::get('/slider/{id}', [SliderController::class, 'show']);
+    
+    
+    Route::get('/counters', [CounterController::class, 'index']);
+    Route::get('/counter/{id}', [CounterController::class, 'show']);
+    
+    Route::get('/settings', [SettingController::class, 'index']);
+    
+    Route::get('/pages', [PageController::class, 'index']);
+    Route::get('/page/{id}', [PageController::class, 'show']);
+    
+    Route::get('/portfolios', [PortfolioController::class, 'index']);
+    Route::get('/portfolio/{id}', [PortfolioController::class, 'show']);
 
-Route::get('/blogs', [BlogController::class, 'index']);
-Route::get('/blog/{id}', [BlogController::class, 'show']);
 
-Route::get('/sliders', [SliderController::class, 'index']);
-Route::get('/slider/{id}', [SliderController::class, 'show']);
+});
 
 
-Route::get('/counters', [CounterController::class, 'index']);
-Route::get('/counter/{id}', [CounterController::class, 'show']);
-
-Route::get('/settings', [SettingController::class, 'index']);
-
-Route::get('/pages', [PageController::class, 'index']);
-Route::get('/page/{id}', [PageController::class, 'show']);
-
-Route::get('/portfolios', [PortfolioController::class, 'index']);
-Route::get('/portfolio/{id}', [PortfolioController::class, 'show']);
 
 Route::post('/contact', [ContactController::class, 'store']);
 
