f44900f98d882068a068a5fb75d9d41e84e37202
boula1997 <boula.nessim.soliman@gmail.com>
2023-08-27 14:15:49 +0300

    edit header in index

--- public/index.php
+++ public/index.php
@@ -5,8 +5,7 @@ use Illuminate\Http\Request;
 
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X- 
-Request-With');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 
 define('LARAVEL_START', microtime(true));
 
