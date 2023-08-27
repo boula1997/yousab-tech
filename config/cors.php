68efa411b918a226008efb162a5f9c653c0500e5
boula1997 <boula.nessim.soliman@gmail.com>
2023-08-27 15:15:43 +0300

    fix cors origin problem

--- config/cors.php
+++ config/cors.php
@@ -14,13 +14,21 @@ return [
     | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
     |
     */
+
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
-    'supportsCredentials' => true,
-    'allowedOrigins' => ['*','https://web.tech.yousab-tech.com/'],
-    'allowedOriginsPatterns' => ['*'],
-    'allowedHeaders' => ['*'],
-    'allowedMethods' => ['*'],
-    'exposedHeaders' => [],
-    'maxAge' => 0,
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['*'],
+
+    'allowed_origins_patterns' => ['*'],
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
 
 ];
