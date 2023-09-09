9a2d3275b9536863d5a89990edaf6e1cb8e41ed6
boula1997 <boula.nessim.soliman@gmail.com>
2023-09-09 13:34:12 +0300

    adding content to  testiomnial

--- resources/views/front/components/testimonial.blade.php
+++ resources/views/front/components/testimonial.blade.php
@@ -1,15 +1,15 @@
-
-    <!-- testimonial-slider start -->
-    <div class="testimonial-slider bg-sky bg-relative">
-        <div class="bg-relative">
-            <div class="slider testimonial-thumb testimonial-thumb-2">
+<!-- testimonial-slider start -->
+<div class="testimonial-slider bg-sky bg-relative">
+    <div class="bg-relative">
+        <div class="slider testimonial-thumb testimonial-thumb-2">
+            @foreach ($testimonials as $testimonial)
                 <div class="item">
                     <div class="row">
                         <div class="col-lg-5 col-md-6">
                             <div class="thumb mb-4 mb-md-0" style="background-image: url('./assets/img/about/1.webp');">
                                 <div class="quote-wrap">
                                     <div class="quote">
-                                        <img src="{{asset('assets/img/testimonial/02.web')}}p" alt="img">
+                                        <img src="{{ asset('assets/img/testimonial/02.web') }}p" alt="img">
                                     </div>
                                 </div>
                             </div>
@@ -18,122 +18,27 @@
                             <div class="single-testimonial-inner text-md-center px-lg-5 px-md-4">
                                 <div class="details">
                                     <div class="thumb">
-                                        <img src="{{asset('assets/img/testimonial/1.webp')}}" alt="img">
+                                        <img src="{{ asset($testimonial->file->url) }}" alt="img">
                                     </div>
-                                    <h2>Donas Foknar</h2>
-                                    <span>Customer,USA</span>
-                                    <p>Eollicitudin nisi nulla eget augue. Maecenas quis turpaliquet, porta lorem et,
-                                        dictum purus. Curabitur vel congue diamamet condimentum metus. Donec feugiat
-                                        fermentposuere. Morbi porttitor augue id </p>
+                                    <h2>{{$testimonial->title}}</h2>
+                                    <span>{{$testimonial->subttitle}}</span>
+                                        {!! $testimonial->description !!}
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="item">
-                    <div class="row">
-                        <div class="col-lg-5 col-md-6">
-                            <div class="thumb mb-4 mb-md-0" style="background-image: url('./assets/img/about/1.webp');">
-                                <div class="quote-wrap">
-                                    <div class="quote">
-                                        <img src="{{asset('assets/img/testimonial/02.web')}}p" alt="img">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-5 col-md-6 align-self-center">
-                            <div class="single-testimonial-inner text-md-center px-lg-5 px-md-4">
-                                <div class="details">
-                                    <div class="thumb">
-                                        <img src="{{asset('assets/img/testimonial/1.webp')}}" alt="img">
-                                    </div>
-                                    <h2>Donas Foknar</h2>
-                                    <span>Customer,USA</span>
-                                    <p>Eollicitudin nisi nulla eget augue. Maecenas quis turpaliquet, porta lorem et,
-                                        dictum purus. Curabitur vel congue diamamet condimentum metus. Donec feugiat
-                                        fermentposuere. Morbi porttitor augue id </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="row">
-                        <div class="col-xl-5 col-md-6">
-                            <div class="thumb mb-4 mb-md-0" style="background-image: url('./assets/img/about/1.webp');">
-                                <div class="quote-wrap">
-                                    <div class="quote">
-                                        <img src="{{asset('assets/img/testimonial/02.web')}}p" alt="img">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-5 col-md-6 align-self-center">
-                            <div class="single-testimonial-inner text-md-center px-lg-5 px-md-4">
-                                <div class="details">
-                                    <div class="thumb">
-                                        <img src="{{asset('assets/img/testimonial/1.webp')}}" alt="img">
-                                    </div>
-                                    <h2>Donas Foknar</h2>
-                                    <span>Customer,USA</span>
-                                    <p>Eollicitudin nisi nulla eget augue. Maecenas quis turpaliquet, porta lorem et,
-                                        dictum purus. Curabitur vel congue diamamet condimentum metus. Donec feugiat
-                                        fermentposuere. Morbi porttitor augue id </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="row">
-                        <div class="col-xl-5 col-md-6">
-                            <div class="thumb mb-4 mb-md-0" style="background-image: url('./assets/img/about/1.webp');">
-                                <div class="quote-wrap">
-                                    <div class="quote">
-                                        <img src="{{asset('assets/img/testimonial/02.web')}}p" alt="img">
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-lg-5 col-md-6 align-self-center">
-                            <div class="single-testimonial-inner text-md-center px-lg-5 px-md-4">
-                                <div class="details">
-                                    <div class="thumb">
-                                        <img src="{{asset('assets/img/testimonial/1.webp')}}" alt="img">
-                                    </div>
-                                    <h2>Donas Foknar</h2>
-                                    <span>Customer,USA</span>
-                                    <p>Eollicitudin nisi nulla eget augue. Maecenas quis turpaliquet, porta lorem et,
-                                        dictum purus. Curabitur vel congue diamamet condimentum metus. Donec feugiat
-                                        fermentposuere. Morbi porttitor augue id </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="slider testimonial-nav-slider testimonial-nav-slider-2">
+            @endforeach
+        </div>
+        <div class="slider testimonial-nav-slider testimonial-nav-slider-2">
+            @foreach ($testimonials as $testimonial)
                 <div class="item">
                     <div class="thumb">
-                        <img src="{{asset('assets/img/testimonial/1.webp')}}" alt="img">
+                        <img src="{{ asset($testimonial->file->url) }}" alt="img">
                     </div>
                 </div>
-                <div class="item">
-                    <div class="thumb">
-                        <img src="{{asset('assets/img/testimonial/2.webp')}}" alt="img">
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="thumb">
-                        <img src="{{asset('assets/img/testimonial/1.webp')}}" alt="img">
-                    </div>
-                </div>
-                <div class="item">
-                    <div class="thumb">
-                        <img src="{{asset('assets/img/testimonial/2.webp')}}" alt="img">
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </div>
     </div>
-    <!-- testimonial-slider end -->
+</div>
+<!-- testimonial-slider end -->
