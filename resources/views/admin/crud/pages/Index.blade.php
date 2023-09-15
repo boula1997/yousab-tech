6359a85fc576034bd78ceb75f41e61c263898426
boula1997 <boula.nessim.soliman@gmail.com>
2023-09-15 17:44:55 +0300

    edit getb image in pages

--- resources/views/admin/crud/pages/Index.blade.php
+++ resources/views/admin/crud/pages/Index.blade.php
@@ -50,7 +50,7 @@
                                                 <tr>
                                                     <td>{{ $loop->iteration }}</td>
                                                     <td><img width="100" height="100"
-                                                            src="{{$page->image:}}"
+                                                            src="{{$page->image}}"
                                                             alt="{{ $page->title }}"></td>
                                                     <td>{{ $page->title }}</td>
                                                     <td>
