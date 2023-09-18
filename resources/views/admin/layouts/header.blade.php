fed86bec9e0424ce768fa4f99b2b56fb2c4a1986
fsd5802 <fsd5802@gmail.com>
2023-09-18 14:17:37 +0300

    remove include custom.css from header

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -38,10 +38,8 @@
     <link rel="stylesheet" href="{{asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css')}}">
     <link rel="stylesheet" href="{{asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css')}}">
     @if (app()->getLocale()=='ar')
-        
     <link rel="stylesheet" href="{{asset('admin/css/custom_ar.css')}}">
     @else
-        
     <link rel="stylesheet" href="{{asset('admin/css/custom.css')}}">
     @endif
     {{-- <link rel="stylesheet" href="{{asset('bootstrap-5.3.1-dist\css\bootstrap.rtl.css')}}"> --}}
