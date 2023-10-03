bad6ab4722d91566d70049f5c28be7bc09e3a3ca
fsd5802 <fsd5802@gmail.com>
2023-10-03 10:22:41 +0300

    adding toastr includes

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -44,6 +44,9 @@
     <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min_ar.css') }}">
     <link rel="stylesheet" href="{{ asset('dist/css/admin_ar.css') }}">
     @endif
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <link rel="stylesheet" type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+
 </head>
