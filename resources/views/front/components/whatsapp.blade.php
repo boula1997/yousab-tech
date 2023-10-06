e995f158924ceaa542e30e4dedf37e0250d38867
boula1997 <boula.nessim.soliman@gmail.com>
2023-10-06 10:39:24 +0300

    fix error in whatsapp

--- resources/views/front/components/whatsapp.blade.php
+++ resources/views/front/components/whatsapp.blade.php
@@ -1,21 +1,4 @@
 <!-- GetButton.io widget -->
-{{-- <script type="text/javascript">
-    (function () {
-        var options = {
-            whatsapp: "01126785910", // WhatsApp number
-            call_to_action: "تواصل معنا", // Call to action
-            button_color: "#0060FF", // Color of button
-            position: "left", // Position may be 'right' or 'left'
-        };
-        var proto = 'https:', host = "getbutton.io", url = proto + '//static.' + host;
-        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
-        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
-        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
-    })();
-</script> --}}
-<!-- /GetButton.io widget -->
-
-<!-- GetButton.io widget -->
 @push('js')
     <script type="text/javascript">
         (function () {
@@ -24,7 +7,7 @@
                 whatsapp: "+201208050298", // WhatsApp number
                 call_to_action: "{{__('general.contact_us')}}", // Call to action
                 button_color: "#0060FF", // Color of button
-                position: "{{ app()->getLocale()=='ar'? 'right' : left }}", // Position may be 'right' or 'left'
+                position: "{{ app()->getLocale()=='ar'? 'right' : 'left' }}", // Position may be 'right' or 'left'
                 order: "call,whatsapp", // Order of buttons
             };
             var proto = 'https:', host = "getbutton.io", url = proto + '//static.' + host;
