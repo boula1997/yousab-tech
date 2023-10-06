3a8879a8f22eb7f8017728f15bc0e14ff951f7b6
ibrahimsamy308 <ibrahimsamy308@gmail.com>
2023-10-06 14:30:47 +0200

    translate update

--- resources/views/admin/components/selectAll.blade.php
+++ resources/views/admin/components/selectAll.blade.php
@@ -4,7 +4,7 @@
             <div
                 class="custom-control custom-switch custom-switch-off-{{ $off }} custom-switch-on-{{ $on }}">
                 <input type="checkbox" class="custom-control-input" value="{{ true }}" id="selectAll">
-                <label class="custom-control-label" id="label" for="selectAll">{{ __('words.selectAll') }}</label>
+                <label class="custom-control-label" id="label" for="selectAll">{{ __('general.selectAll') }}</label>
             </div>
         </div>
     </div>
@@ -16,9 +16,9 @@
         $(document).ready(function() {
             $("#selectAll").on('click', function() {
                 if (this.checked) {
-                    $('#label').text("{{ __('words.dselectAll') }}");
+                    $('#label').text("{{ __('general.dselectAll') }}");
                 } else {
-                    $('#label').text("{{ __('words.selectAll') }}");
+                    $('#label').text("{{ __('general.selectAll') }}");
                 }
 
                 $('input:checkbox').not(this).prop('checked', this.checked);
