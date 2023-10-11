ef5e879fa89967f1c8397e9a29d87d77436fc3ae
fsd5802 <fsd5802@gmail.com>
2023-10-11 16:48:41 +0300

    adding toogle delte

--- resources/views/admin/components/controls.blade.php
+++ resources/views/admin/components/controls.blade.php
@@ -28,3 +28,49 @@
         @endcan
     </form>
 @endif
+
+{{-- toodle delete --}}
+{{-- @if(auth('admin')->user()->hasPermission('delete-' . $role) || auth('admin')->user()->hasPermission('delete_subscribed_users-' . $role))
+<form id="delete-form-{{ $value->id }}" style="display: inline-table;"
+      action="{{ route($name . '.destroy', $value->id) }}" method="post">
+    @csrf
+    @method('delete')
+
+    <button type="button" class="btn btn-sm btn-clean btn-icon m-1" title="{{__('words.delete')}}"
+            data-toggle="modal" data-target="#deleteModalSizeSm-{{ $value->id }}">
+    <span class="svg-icon svg-icon-md svg-icon-primary">
+        <!--begin::Svg Icon | path:assets/media/svg/icons/General/Trash.svg-->
+        <i class="fa fa-trash"></i>
+    </span>
+    </button>
+    <div class="modal fade" id="deleteModalSizeSm-{{$value->id}}" tabindex="-1" role="dialog"
+         aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h4 class="modal-title">{{__('words.delete_confirm')}}</h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body"> {{ __('message.delete_message') }} <b
+                        class="">
+                        @if($value->name)
+                            {{$value->name}}
+                        @elseif($value->title)
+                            {!! $value->title !!}
+                        @else
+                            {{$value->email}}
+                        @endif
+                    </b></div>
+                <div class="modal-footer">
+                    <button type="button" class="btn gray btn-outline-secondary"
+                            data-dismiss="modal">{{__('words.cancel')}}</button>
+                    <button type="submit" class="btn gray btn-outline-danger">{{__('words.delete')}}</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</form>
+@endif
+@endif --}}
