db7d4b2d168432483dd1179c4609d74c4b75e423
boula1997 <boula.nessim.soliman@gmail.com>
2024-05-10 19:23:24 +0300

    commit

--- resources/views/front/components/portfolios.blade.php
+++ resources/views/front/components/portfolios.blade.php
@@ -3,26 +3,23 @@
         <div class="container">
             <div class="row">
                 @foreach ($portfolios as $portfolio)
-
-                <div class="col-lg-4 col-md-6">
-                    <div class="single-team-inner style-4 text-center">
-                        <div class="thumb">
-                            <img src="{{ $portfolio->image }}" alt="img">
-                        </div>
-                        <div class="details-wrap">
-                            <div class="details-inner">
-                                <h4><a href="team-details.html">{{ $portfolio->title }}</a></h4>
-                                <p>{{ $portfolio->title }}</p>
-                                <a href="{{ route('front.show.portfolio',$portfolio->id) }}">
-                                <button class="btn btn-primary">{{ __('general.show') }}</button>
-                                </a>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="single-team-inner style-4 text-center">
+                            <div class="thumb">
+                                <img src="{{ $portfolio->image }}" alt="img">
+                            </div>
+                            <div class="details-wrap">
+                                <div class="details-inner">
+                                    <h4><a href="team-details.html">{{ $portfolio->title }}</a></h4>
+                                    <p>{{ $portfolio->title }}</p>
+                                    <a href="{{ route('front.show.portfolio', $portfolio->id) }}">
+                                        <button class="btn btn-primary">{{ __('general.show') }}</button>
+                                    </a>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                    
                 @endforeach
-            
             </div>
         </div>
     </div>
