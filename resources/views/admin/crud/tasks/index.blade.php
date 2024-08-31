65b583efa044c7b898f6c6c44a528fd592629290
boula1997 <boula.nessim.soliman@gmail.com>
2024-08-31 09:25:20 +0300

    commit

--- resources/views/admin/crud/tasks/index.blade.php
+++ resources/views/admin/crud/tasks/index.blade.php
@@ -111,6 +111,7 @@
             "responsive": true,
             "lengthChange": false,
             "autoWidth": false,
+            "paging": false,
             "searching": true,
             "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
         }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
