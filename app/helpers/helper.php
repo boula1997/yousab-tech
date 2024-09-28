014f3901e260e93f22822e22638d7694c32683cc
boula1997 <boula.nessim.soliman@gmail.com>
2024-09-29 00:19:34 +0300

    commit

--- app/helpers/helper.php
+++ app/helpers/helper.php
@@ -133,6 +133,7 @@ function rest($project)
 {
     $totalFee=0;
     foreach($project->fees as $fee){
+        if($fee>0)
         $totalFee+=$fee->amount;
     }
     return $project->cost-$totalFee;
