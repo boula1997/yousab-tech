f4e776d3e7078eb06293731583936d701cbb71cb
Boula Nessim <boula.nessim.soliman@gmail.com>
2025-01-21 14:52:53 +0200

    commit

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="en" dir="{{app()->getLocale()=="ar"?"rtl":"ltr"}}">
 
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
