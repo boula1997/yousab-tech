c7e3148873f58bfcd2c077d2286d9c338ede1380
Boula <boula.nessim.soliman@gmail.com>
2025-05-28 02:23:52 +0300

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -30,7 +30,7 @@
 
     <!-- login section start -->
     <section class="log-in-section section-b-space" style="background-image:url({{settings()->image}});">
-        <a href="" class="logo-login"><img src="{{ettings()->logo}}" class="img-fluid"></a>
+        <a href="" class="logo-login"><img src="{{settings()->logo}}" class="img-fluid"></a>
         <div class="container w-100">
             <div class="row">
 
